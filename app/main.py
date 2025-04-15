c999a12d6bd12fa3d68f51c158a8f69fc0793461
RomanSapunGit <sapunroma23@gmail.com>
2025-04-15 14:40:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,71 @@
-class Validator:
-    pass
+from abc import abstractmethod, ABC
+from typing import Any
 
 
-class Number:
-    pass
+class Validator(ABC):
+    @abstractmethod
+    def validate(self, value: Any) -> None:
+        pass
+
+    def __set_name__(self, owner: type, name: str) -> None:
+        self.protected_name = f"_{name}"
+
+    def __get__(self, instance: Any, owner: type) -> Any:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: Any, value: Any) -> None:
+        self.validate(value)
+        setattr(instance, self.protected_name, value)
 
 
-class OneOf:
-    pass
+class Number(Validator):
+    def __init__(self, min_value: int, max_value: int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value: Any) -> None:
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer.")
+        if self.min_value > value or value > self.max_value:
+            raise ValueError(f"Quantity should not be less than "
+                             f"{self.min_value} and greater than "
+                             f"{self.max_value}.")
+
+
+class OneOf(Validator):
+    def __init__(self, options: list[str]) -> None:
+        self.options = options
+
+    def validate(self, value: Any) -> None:
+        if value not in self.options:
+            raise ValueError(f"Expected {value} to be one of"
+                             f" {tuple(self.options)}.")
 
 
 class BurgerRecipe:
-    pass
+    buns: Number = Number(2, 3)
+    cheese: Number = Number(0, 2)
+    tomatoes: Number = Number(0, 3)
+    cutlets: Number = Number(1, 3)
+    eggs: Number = Number(0, 2)
+    sauce: OneOf = OneOf(["ketchup", "mayo", "burger"])
+
+    def __init__(
+            self,
+            buns: int,
+            cheese: int,
+            tomatoes: int,
+            cutlets: int,
+            eggs: int,
+            sauce: str
+    ) -> None:
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
+
+burger = BurgerRecipe(buns=2, cheese=1, tomatoes=1, cutlets=1, eggs=1, sauce="ketchup")
+print(burger.__dict__)
+print(burger.sauce)
